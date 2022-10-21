a30e7104b3f6eeafdada9b637725257e00cf7d98
ThisIsBeans <brianvm9@gmail.com>
2022-10-21 12:14:17 -0700

    rtupdate 10/21
    begin working on our team section

--- js/script.js
+++ js/script.js
@@ -1,23 +1,2 @@
-function headerClick(elem) {
-    if (elem.innerHTML === 'top') {
-      document.getElementById("top").scrollIntoView();
-      window.scrollBy(0, -(elem.offsetHeight))
-    }
-    if (elem.innerHTML === 'about') {
-      document.getElementById("about").scrollIntoView();
-      window.scrollBy(0, -(elem.offsetHeight))
-      }
-    if (elem.innerHTML === 'services') {
-      document.getElementById("services").scrollIntoView();
-      window.scrollBy(0, -(elem.offsetHeight)-2)
-      }
-    if (elem.innerHTML === 'Four') {
-      document.getElementById("Four").scrollIntoView();
-      window.scrollBy(0, -(elem.offsetHeight)-2)
-  
-    }
-    if (elem.innerHTML === 'Five') {
-      document.getElementById("Five").scrollIntoView();
-      window.scrollBy(0, -(elem.offsetHeight)-2)
-    }
-  }
+const foo = { name: 'tom', age: 30, nervous: false };
+console.log(foo);
