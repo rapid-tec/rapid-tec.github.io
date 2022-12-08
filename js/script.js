25a4a7e6e5e9da4765253c6b88c9f3922d093540
ThisIsBeans <brianvm9@gmail.com>
2022-12-08 14:00:15 -0800

    messed up

--- js/script.js
+++ js/script.js
@@ -1,69 +1,17 @@
-// Start autoplaying automatically
-var autoplayInterval = setInterval(function() {
+const images = ["img/fields1.png", "img/fields2.jpg", "img/fields3.jpg", "img/fields4.jpg", "img/fields5.jpg"];
+const carousel = document.querySelector(".carousel");
+const interval = setInterval(function() {
+  startCarousel();
+}, 3000);
+var index = 1;
 
-    // Get element via id and click next
-    document.getElementById("next").click();
-    
-   
-  }, 1000); // Do this every 1 second, increase this!
-
-// Stop function added to button
-function stopAutoplay() {
-
-  // Stop the autoplay
-  clearInterval(autoplayInterval);
-
-}
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Start autoplaying automatically
-var autoplayInterval = setInterval(function() {
-
-    // Get element via id and click next
-    document.getElementById("next").click();
-    
-   
-  }, 1000); // Do this every 1 second, increase this!
-
-// Stop function added to button
-function stopAutoplay() {
-
-  // Stop the autoplay
-  clearInterval(autoplayInterval);
-
-}
+startCarousel = () => {
+  carousel.style.backgroundImage = `url(${images[index++]})`;
+  carousel.classList.remove("fade");
+  void carousel.offsetWidth;
+  carousel.classList.add("fade");
+  if(index > images.length -1) index = 0;
+};
 
 
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {
-    slideIndex = 1
-  }
-  if (n < 1) {
-    slideIndex = slides.length
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
-}
